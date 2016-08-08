eafeeaba4fbcf5438af4d10a9eb5aee426e8c78f
LimpLungs <xlaxlimplungs@gmail.com>
2016-08-08 16:43:36 -0500

    initial commit for LimpCraft version 3.0dev1

---
+++ src/main/java/com/limplungs/limpcraft/LimpCraft.java
@@ -0,0 +1,18 @@
+package com.limplungs.limpcraft;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = LimpCraft.MODID, version = LimpCraft.VERSION)
+public class LimpCraft
+{
+    public static final String MODID = "limpcraft";
+    public static final String VERSION = "3.0dev1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+}
